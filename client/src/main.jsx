2f6a4e4ac99433ddfaa47ed2568ce2cfaadc702d
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-02-03 01:28:29 +1100

    added basic page router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./pages/Home.jsx"
+import Signup from "./pages/SignupForm.jsx"
+import Error from "./pages/Error.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);

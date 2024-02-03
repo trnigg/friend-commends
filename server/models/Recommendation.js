e9a75198e1b3930dac139bea3c9dbf5fbfbd0513
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-02-03 20:39:01 +1100

    Implement recommendations model for mongoDB

---
+++ server/models/Recommendation.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require('mongoose');
+
+
+const recommendationSchema = new Schema(
+  {
+    tmdbID:{
+        type: String,
+        required: true,
+    },
+    type:{
+        type: String,
+        required: true,
+    },
+    original_title: {
+        type: String,
+        required: false,
+    },
+    original_name: {
+        type: String,
+        required: false,
+    },
+    poster_path: {
+      type: String,
+      required: false,
+    },
+    overview: {
+        type: String,
+        required: false,
+    },
+    release_date: {
+        type: Date,
+        required: false,
+    },
+    first_air_date: {
+        type: Date,
+        required: false,
+    },
+    AU_platforms:{
+        type:[String],
+        required:false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters:true,
+    },
+  }
+);
+
+//count 
+// recommendationSchema.virtual('Count').get(function () {
+//     return this.reactions.length;
+//   });
+
+
+
+const Recommendation = model('recommendation', recommendationSchema);
+
+module.exports = Recommendation;

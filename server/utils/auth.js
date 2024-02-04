dca98b00d405e8649503d45fbb6166aece1ab271
trnigg <tim@nigg.com.au>
2024-02-05 10:35:11 +1100

    Updates server-side auth to use environment variable for secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,10 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh'; // UPDATE TO USE ENV VAR
+const path = require('path'); // required to resolve the path to the .env file
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }); // Getting secret from .env file in root directory
+
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
@@ -35,7 +38,7 @@ module.exports = {
 	// WHAT ELSE DO WE WAN IN THE PAYLOAD?
 	signToken: function ({ email, _id }) {
 		const payload = { email, _id };
-
+		console.log(secret, expiration, payload); // TODO - remove!
 		return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 	},
 };

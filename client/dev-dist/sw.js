004583e71bebfcc7f59e66d5c5689455d18dc1ab
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-02-04 22:41:08 +1100

    needed to add code to vite.config.js to allow access of API call from front end

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f3bf6504'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fkrgs83c978"
+    "revision": "0.a5dl0cqcbeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,10 +1,12 @@
 import RejectRequest from '../components/rejectRequest';
+import Searchbar from '../components/searchBar';
 
 function Overview(){
     return(
         <div>
             <h1>Overview</h1>
             <br />
+            <Searchbar />
             <RejectRequest />
         </div>
     )

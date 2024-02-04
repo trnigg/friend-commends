fc03ae0cc2be5b48ba6290bf853742f6fa161c68
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-02-04 14:55:46 +1100

    restructure graphql files and filename

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,12 @@
-const typeDefs = require('./typeDefs');
+const { mergeTypeDefs } = require('@graphql-tools/merge')
+
+const userTypeDefs = require('./userTypeDefs');
+const recommendationTypeDefs = require('./recommendationTypeDefs');
+
+const typeDefs = mergeTypeDefs([userTypeDefs, recommendationTypeDefs])
+
 const resolvers = require('./resolvers');
+
+
 
 module.exports = { typeDefs, resolvers };

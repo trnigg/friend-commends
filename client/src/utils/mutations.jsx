39dfe938eeb10b4acefc273e4d96d7be5c2ab9ec
trnigg <tim@nigg.com.au>
2024-02-05 13:14:20 +1100

    Adds mutations for accepting/rejecting friend requests

--- client/src/utils/mutations.jsx
+++ client/src/utils/mutations.jsx
@@ -1,82 +1,128 @@
 import { gql } from '@apollo/client';
 
 export const MUTATION_LOGIN = gql`
-    mutation Login($input: LoginInput!) {
-        login(input: $input) {
-        token
-        user {
-            firstName
-            lastName
-            userName
-            id
-        }
-        }
-    }
+	mutation Login($input: LoginInput!) {
+		login(input: $input) {
+			token
+			user {
+				firstName
+				lastName
+				userName
+				id
+			}
+		}
+	}
 `;
 
 export const MUTATION_ADDUSER = gql`
-    mutation AddUser($input: UserInput!) {
-        addUser(input: $input) {
-        token
-        user {
-            id
-            firstName
-            lastName
-            email
-            password
-            dateOfBirth
-        }
-        }
-    }
+	mutation AddUser($input: UserInput!) {
+		addUser(input: $input) {
+			token
+			user {
+				id
+				firstName
+				lastName
+				email
+				password
+				dateOfBirth
+			}
+		}
+	}
 `;
 
 export const MUTATION_DELETEUSER = gql`
-    mutation deleteUser($deleteUserId: ID!) {
-        deleteUser(id: $deleteUserId) {
-        id
-        userName
-        }
-    }
+	mutation deleteUser($deleteUserId: ID!) {
+		deleteUser(id: $deleteUserId) {
+			id
+			userName
+		}
+	}
 `;
 
 export const MUTATION_SENDREQUEST = gql`
-    mutation sendFriendRequest($fromUserId: ID!, $toUserId: ID!) {
-        sendFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
-        userName
-        sentFriendRequests {
-            userName
-            firstName
-            lastName
-        }
-        }
-    }
+	mutation sendFriendRequest($fromUserId: ID!, $toUserId: ID!) {
+		sendFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
+			userName
+			sentFriendRequests {
+				userName
+				firstName
+				lastName
+			}
+		}
+	}
 `;
 
 export const MUTATION_ACCEPTREQUEST = gql`
-mutation acceptFriendRequest($fromUserId: ID!, $toUserId: ID!) {
-    acceptFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
-      id
-      userName
-      firstName
-      friends {
-        id
-        userName
-        firstName
-      }
-    }
-  }
+	mutation acceptFriendRequest($fromUserId: ID!, $toUserId: ID!) {
+		acceptFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
+			id
+			userName
+			firstName
+			friends {
+				id
+				userName
+				firstName
+			}
+		}
+	}
 `;
 
 export const MUTATION_REJECTREQUEST = gql`
-mutation rejectFriendRequest($fromUserId: ID!, $toUserId: ID!) {
-    rejectFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
-      id
-      userName
-      firstName
-      pendingFriendRequests {
-        id
-        userName
-      }
-    }
-  }
+	mutation rejectFriendRequest($fromUserId: ID!, $toUserId: ID!) {
+		rejectFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
+			id
+			userName
+			firstName
+			pendingFriendRequests {
+				id
+				userName
+			}
+		}
+	}
+`;
+
+export const ACCEPT_FRIEND_REQUEST = gql`
+	mutation AcceptFriendRequest($fromUserId: ID!, $toUserId: ID!) {
+		acceptFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
+			id
+			userName
+			firstName
+			lastName
+			friends {
+				id
+				userName
+				firstName
+				lastName
+			}
+			pendingFriendRequests {
+				id
+				userName
+				firstName
+				lastName
+			}
+		}
+	}
+`;
+
+export const REJECT_FRIEND_REQUEST = gql`
+	mutation RejectFriendRequest($fromUserId: ID!, $toUserId: ID!) {
+		rejectFriendRequest(fromUserId: $fromUserId, toUserId: $toUserId) {
+			id
+			userName
+			firstName
+			lastName
+			friends {
+				id
+				userName
+				firstName
+				lastName
+			}
+			pendingFriendRequests {
+				id
+				userName
+				firstName
+				lastName
+			}
+		}
+	}
 `;

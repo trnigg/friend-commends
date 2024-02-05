2f9e2053c435f18798790d0a07d89967694fe69d
trnigg <tim@nigg.com.au>
2024-02-06 09:47:21 +1100

    Refactors queries.jsx to include additional user information and friend status

--- client/src/utils/queries.jsx
+++ client/src/utils/queries.jsx
@@ -1,70 +1,83 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ALL = gql`
-    query Users {
-        users {
-        firstName
-        id
-        lastName
-        email
-        password
-        dateOfBirth
-        userName
-        }
-    }
+	query Users {
+		users {
+			id
+			userName
+			firstName
+			lastName
+			email
+			friends {
+				id
+			}
+			pendingFriendRequests {
+				id
+			}
+			sentFriendRequests {
+				id
+			}
+		}
+	}
 `;
 
 export const QUERY_USER = gql`
-query user($userId: ID!) {
-    user(id: $userId) {
-      id
-      userName
-      firstName
-      lastName
-      email
-      password
-      dateOfBirth
-      friends {
-        id
-        userName
-        firstName
-        lastName
-      }
-      pendingFriendRequests {
-        id
-        userName
-        firstName
-        lastName
-      }
-      sentFriendRequests {
-        id
-        userName
-        firstName
-        lastName
-      }
-      recommendations {
-        ... on Movie {
-          id
-          original_title
-          tmdbID
-        }
-        ... on TV {
-          id
-          original_name
-          tmdbID
-        }
-      }
-    }
-  }  
+	query user($userId: ID!) {
+		user(id: $userId) {
+			id
+			userName
+			firstName
+			lastName
+			email
+			password
+			dateOfBirth
+			friends {
+				id
+				userName
+				firstName
+				lastName
+			}
+			pendingFriendRequests {
+				id
+				userName
+				firstName
+				lastName
+			}
+			sentFriendRequests {
+				id
+				userName
+				firstName
+				lastName
+			}
+			recommendations {
+				... on Movie {
+					id
+					original_title
+					tmdbID
+				}
+				... on TV {
+					id
+					original_name
+					tmdbID
+				}
+			}
+		}
+	}
 `;
 
 export const QUERY_FRIENDS = gql`
-    query friends($friendsId: ID!) {
-        friends(id: $friendsId) {
-        id
-        userName
-        firstName
-        lastName
-        }
-    }
+	query friends($friendsId: ID!) {
+		friends(id: $friendsId) {
+			id
+			userName
+			firstName
+			lastName
+		}
+	}
+`;
+
+export const QUERY_FRIEND_STATUS = gql`
+	query FriendStatus($userId: ID!, $friendId: ID!) {
+		friendStatus(userId: $userId, friendId: $friendId)
+	}
 `;

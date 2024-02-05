eaa078d0f7704b85651d653a0fd3cf6cd4d51357
trnigg <tim@nigg.com.au>
2024-02-06 09:02:14 +1100

    Adds conditional rendering to user search results

--- client/src/components/userSearchBar.jsx
+++ client/src/components/userSearchBar.jsx
@@ -1,13 +1,15 @@
 // TODO if time - debounce (delay) search requests to avoid making a request for every keystroke
 // TODO - fix query firing on every keystroke
 // may need to use useCallback to memoize the search function https://react.dev/reference/react/useCallback
+// Currently every key stroke is returning ALL users, not just matching search - this is not currently an issue until we have more data
+// Similarly, pagination will be needed to handle large numbers of users
 
 // https://react.semantic-ui.com/modules/search/#variations-fluid
 import { useState, useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
+import { useLazyQuery, useQuery } from '@apollo/client';
 import _ from 'lodash'; // lodash allows for debouncing (delaying) of search requests and escaping special characters in regex
 import { Search, Button, Icon } from 'semantic-ui-react';
-import { QUERY_ALL } from '../utils/queries';
+import { QUERY_ALL, QUERY_FRIEND_STATUS } from '../utils/queries';
 import AuthService from '../utils/auth';
 
 function UserSearchBar() {
@@ -48,47 +50,83 @@ function UserSearchBar() {
 	// IF user has already sent a friend request to the user, display "Request Sent" instead of button
 	// TODO : user's number of recommendations
 	// TO
+
+	// Configure how to render search results
 	const resultRenderer = ({
+		id: friendId,
 		firstName,
 		lastName,
 		userName,
-		// recommendations,
-		// profileImage,
-	}) => (
-		<div
-			style={{
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'space-between',
-			}}
-		>
-			<div style={{ display: 'flex', alignItems: 'center' }}>
-				<img
-					src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-					alt={`${firstName} ${lastName}`}
-					style={{
-						marginRight: '10px',
-						width: '50px',
-						height: '50px',
-						borderRadius: '50%',
-					}}
-				/>
-				<div>
+		sentFriendRequests,
+		pendingFriendRequests,
+		friends,
+		// The following are used to determine the status of the friend request and display the appropriate button/message,
+		// by checking if the user's id appears in the friends, sentFriendRequests, or pendingFriendRequests arrays of the user being searched
+	}) => {
+		let friendStatus;
+		if (friends.some((friend) => friend.id === userId)) {
+			friendStatus = 'FRIENDS';
+		} else if (sentFriendRequests.some((request) => request.id === userId)) {
+			friendStatus = 'REQUEST_RECEIVED';
+		} else if (pendingFriendRequests.some((request) => request.id === userId)) {
+			friendStatus = 'REQUEST_SENT';
+		} else {
+			friendStatus = 'NOT_FRIENDS';
+		}
+
+		return (
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				}}
+			>
+				<div style={{ display: 'flex', alignItems: 'center' }}>
+					<img
+						src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+						alt={`${firstName} ${lastName}`}
+						style={{
+							marginRight: '10px',
+							width: '50px',
+							height: '50px',
+							borderRadius: '50%',
+						}}
+					/>
 					<div>
-						{firstName} {lastName}
-					</div>
-					<div>{userName}</div>
-					<div>
-						<strong>0</strong> recommendations
+						<div>
+							{firstName} {lastName}
+						</div>
+						<div>{userName}</div>
+						<div>
+							<strong>0</strong> recommendations
+						</div>
 					</div>
 				</div>
+				{friendStatus === 'FRIENDS' ? (
+					<Button disabled>
+						<Icon name="user" />
+						Already Friends
+					</Button>
+				) : friendStatus === 'REQUEST_SENT' ? (
+					<Button disabled>
+						<Icon name="user outline" />
+						Request Sent
+					</Button>
+				) : friendStatus === 'REQUEST_RECEIVED' ? (
+					<Button basic color="green">
+						<Icon name="user plus" />
+						Accept their Request
+					</Button>
+				) : (
+					<Button>
+						<Icon name="user plus" />
+						Send Friend Request
+					</Button>
+				)}
 			</div>
-			<Button>
-				<Icon name="user plus" />
-				Send Friend Request
-			</Button>
-		</div>
-	);
+		);
+	};
 
 	// Need to use resultRenderer function to define how to render search results
 	// https://react.semantic-ui.com/modules/search/

0316cd2fe9b82c6c786f09f20bc0d534c55337d5
trnigg <tim@nigg.com.au>
2024-02-06 16:09:36 +1100

    Adds new pages as per wireframe: Books, ForYou, Movies, TVShows, and User

---
+++ client/src/pages/ForYou.jsx
@@ -0,0 +1,9 @@
+function ForYou() {
+	return (
+		<div>
+			<h1>This is the ForYou page</h1>
+		</div>
+	);
+}
+
+export default ForYou;
---
+++ client/src/pages/Movies.jsx
@@ -0,0 +1,9 @@
+function Movies() {
+	return (
+		<div>
+			<h1>This is the Movies page</h1>
+		</div>
+	);
+}
+
+export default Movies;

20771df6cbb6c73460de46f6f7b33cf47db5fbbd
trnigg <tim@nigg.com.au>
2024-02-06 15:54:56 +1100

    Renames import sto match new names

--- client/src/components/logNewUser.jsx
+++ client/src/components/logNewUser.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
-import Login from "./login";
-import AddUser from "./createNewUser";
+import { useState } from 'react';
+import Login from './login';
+import AddUser from './signUp';
 
-function LogNewUser(){
-    const [logStatus, setLogStatus] = useState('login')
+function LogNewUser() {
+	const [logStatus, setLogStatus] = useState('login');
 
-    const updateStatus = (data) =>{
-        setLogStatus(data);
-    }
+	const updateStatus = (data) => {
+		setLogStatus(data);
+	};
 
-    return(
-        <div>
-            {(logStatus==='login') ? 
-            <Login onSubmit={updateStatus}/> :
-            <AddUser onSubmit={updateStatus}/>}
-        </div>
-    )
+	return (
+		<div>
+			{logStatus === 'login' ? (
+				<Login onSubmit={updateStatus} />
+			) : (
+				<AddUser onSubmit={updateStatus} />
+			)}
+		</div>
+	);
 }
 
 export default LogNewUser;

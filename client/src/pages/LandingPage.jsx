8a606b4dbffc423cd76d67120c8b7e255eb7d993
trnigg <tim@nigg.com.au>
2024-02-06 15:51:27 +1100

    Updates LandingPage component with placeholder-logo

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
-// TODO in CSS: Format so component stack vertically on mobile
-// Breakpoint so that elements stack but are next to forms on desktop
+// TODO in CSS: Format with breakpoints so component stack vertically on mobile, and horizontally on larger screens.
+// TODO: Use more semantic HTML elements where possible
+// TODO: Update logo and remove inline styles, update alt text
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthService from '../utils/auth';
 import Login from '../components/login'; // Import your Login component
 import Signup from '../components/signUp'; // Import your Signup component
+
+// TODO: Replace logo with our logo. Preferably a .svg file or png with transparent background
+import Logo from '../../public/images/logo.jpg';
 
 function LandingPage({ setIsLoggedIn }) {
 	const navigate = useNavigate();
@@ -29,13 +33,22 @@ function LandingPage({ setIsLoggedIn }) {
 	};
 
 	return (
-		<div>
-			<h1>Welcome to Our App</h1>
-			<p>This is a description of our app.</p>
+		<div className="landing-page">
+			<div className="text-content">
+				<h1>Welcome to Our App</h1>
+				<img
+					src={Logo}
+					alt="Logo"
+					className="logo"
+					style={{ width: '300px', height: 'auto' }}
+				/>{' '}
+				{/* Add your logo here */}
+				<p>This is a description of our app.</p>
+			</div>
 			{isLogin ? (
-				<Login onSignupClick={toggleLoginSignup} handleAuth={handleAuth} /> // Render Login component if isLogin is true
+				<Login onSignupClick={toggleLoginSignup} handleAuth={handleAuth} />
 			) : (
-				<Signup onLoginClick={toggleLoginSignup} handleAuth={handleAuth} /> // Render Signup component if isLogin is false
+				<Signup onLoginClick={toggleLoginSignup} handleAuth={handleAuth} />
 			)}
 		</div>
 	);

2e38422beea40433c64b9d5c0302c40a380a2b14
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-02-08 03:05:44 +1100

    add a user profile card on User page

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import WatchList from "../components/watchList";
 import ShareReceivedList from "../components/shareReceivedList";
+import RecommendationList from "../components/recommendationList";
+import UserCard from "../components/userProfile";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
 import AuthService from "../utils/auth";
 
 import { Header } from "semantic-ui-react";
 import {
-	AccordionTitle,
-	AccordionContent,
-	Accordion,
-	Icon,
-  } from 'semantic-ui-react'
+  AccordionTitle,
+  AccordionContent,
+  Accordion,
+  Icon,
+} from "semantic-ui-react";
 
 function User() {
   const { data: { _id: userId } = {} } = AuthService.getProfile();
@@ -22,52 +24,60 @@ function User() {
   });
   //console.log(userId);
   //console.log("data:",data);
-  if (loading) return 'Loading...';
+  if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
- 
- 
 
-  const handleClick = (event,titleProps) => {
-	console.log("event",event);
-	console.log("titleProps",titleProps);
-	const { index } = titleProps;
+  const handleClick = (event, titleProps) => {
+    //console.log("event", event);
+    //console.log("titleProps", titleProps);
+    const { index } = titleProps;
     const newIndex = activeIndex === index ? -1 : index;
 
     setActiveIndex(newIndex);
-  }
-
+  };
 
   return (
     <div>
       <Header as="h1">Hi {data.user.firstName} </Header>
 
-      <div>
-        
-		<Accordion fluid styled>
-        <AccordionTitle
-          active={activeIndex === 0}
-          index={0}
-          onClick={handleClick}
-        >
-          <Icon name='dropdown' />
-          Your Watch List:
-        </AccordionTitle>
-        <AccordionContent active={activeIndex === 0}>
-		<WatchList />
-        </AccordionContent>
-        <AccordionTitle
-          active={activeIndex === 1}
-          index={1}
-          onClick={handleClick}
-        >
-          <Icon name='dropdown' />
-          Shares received List:
-        </AccordionTitle>
-        <AccordionContent active={activeIndex === 1}>
-		<ShareReceivedList />
-        </AccordionContent>
-		</Accordion>
+      <UserCard data={data.user} />
 
+      <div>
+        <Accordion fluid styled>
+          <AccordionTitle
+            active={activeIndex === 0}
+            index={0}
+            onClick={handleClick}
+          >
+            <Icon name="dropdown" />
+            Your Watch List:
+          </AccordionTitle>
+          <AccordionContent active={activeIndex === 0}>
+            <WatchList />
+          </AccordionContent>
+          <AccordionTitle
+            active={activeIndex === 1}
+            index={1}
+            onClick={handleClick}
+          >
+            <Icon name="dropdown" />
+            Shares received List:
+          </AccordionTitle>
+          <AccordionContent active={activeIndex === 1}>
+            <ShareReceivedList />
+          </AccordionContent>
+          <AccordionTitle
+            active={activeIndex === 2}
+            index={2}
+            onClick={handleClick}
+          >
+            <Icon name="dropdown" />
+            Your recommendations:
+          </AccordionTitle>
+          <AccordionContent active={activeIndex === 2}>
+            <RecommendationList />
+          </AccordionContent>
+        </Accordion>
       </div>
     </div>
   );

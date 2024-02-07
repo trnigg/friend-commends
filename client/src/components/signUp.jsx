e99124794797146750d75cea3804cac04d00d2fa
trnigg <tim@nigg.com.au>
2024-02-07 11:05:48 +1100

    Adds form validation and error handling for empty fields using Semantic

--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,3 +1,5 @@
+// TODO: Add global form validation message
+
 import { useState, useEffect } from 'react';
 import { Button, Form, FormField, Message } from 'semantic-ui-react';
 import { MUTATION_ADDUSER } from '../utils/mutations';
@@ -6,11 +8,17 @@ import { useMutation } from '@apollo/client';
 function SignUp({ onAuthenticated }) {
 	const [addUser, { error, data }] = useMutation(MUTATION_ADDUSER);
 	const [userName, setUserName] = useState('');
+	const [userNameError, setUserNameError] = useState(false);
 	const [firstName, setFirstName] = useState('');
+	const [firstNameError, setFirstNameError] = useState(false);
 	const [lastName, setLastName] = useState('');
+	const [lastNameError, setLastNameError] = useState(false);
 	const [email, setEmail] = useState('');
+	const [emailError, setEmailError] = useState(false);
 	const [password, setPassword] = useState('');
+	const [passwordError, setPasswordError] = useState(false);
 	const [dob, setDob] = useState('');
+	const [dobError, setDobError] = useState(false);
 
 	useEffect(() => {
 		if (error) {
@@ -23,6 +31,27 @@ function SignUp({ onAuthenticated }) {
 
 	const handleFormSubmit = async (e) => {
 		e.preventDefault();
+
+		// Reset error states
+		setUserNameError(false);
+		setFirstNameError(false);
+		setLastNameError(false);
+		setEmailError(false);
+		setPasswordError(false);
+		setDobError(false);
+
+		// Check if any field is empty
+		if (!userName) setUserNameError(true);
+		if (!firstName) setFirstNameError(true);
+		if (!lastName) setLastNameError(true);
+		if (!email) setEmailError(true);
+		if (!password) setPasswordError(true);
+		if (!dob) setDobError(true);
+
+		// If any field is empty, stop form submission
+		if (!userName || !firstName || !lastName || !email || !password || !dob)
+			return;
+
 		try {
 			await addUser({
 				variables: {
@@ -41,6 +70,7 @@ function SignUp({ onAuthenticated }) {
 		}
 	};
 
+	// Using Semantic React built in form valdiation for each field
 	return (
 		<div>
 			<h2>Sign up</h2>
@@ -52,60 +82,84 @@ function SignUp({ onAuthenticated }) {
 				/>
 			)}
 			<Form onSubmit={handleFormSubmit}>
-				<FormField>
-					<label htmlFor="userName">Username</label>
-					<Form.Input
-						type="text"
-						id="userName"
-						value={userName}
-						onChange={(e) => setUserName(e.target.value)}
-					/>
-				</FormField>
-				<FormField>
-					<label htmlFor="firstName">First Name</label>
-					<Form.Input
-						type="text"
-						id="firstName"
-						value={firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-				</FormField>
-				<FormField>
-					<label htmlFor="lastName">Last Name</label>
-					<Form.Input
-						type="text"
-						id="lastName"
-						value={lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
-				</FormField>
-				<FormField>
-					<label htmlFor="email">Email</label>
-					<Form.Input
-						type="email"
-						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</FormField>
-				<FormField>
-					<label htmlFor="password">Password</label>
-					<Form.Input
-						type="password"
-						id="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</FormField>
-				<FormField>
-					<label htmlFor="dob">Date of Birth</label>
-					<Form.Input
-						type="date"
-						id="dob"
-						value={dob}
-						onChange={(e) => setDob(e.target.value)}
-					/>
-				</FormField>
+				<FormField
+					id="form-input-control-username"
+					control={Form.Input}
+					label="Username"
+					placeholder="Username"
+					value={userName}
+					onChange={(e) => setUserName(e.target.value)}
+					error={
+						userNameError
+							? { content: 'Please enter a username', pointing: 'below' }
+							: null
+					}
+				/>
+				<FormField
+					id="form-input-control-first-name"
+					control={Form.Input}
+					label="First Name"
+					placeholder="First Name"
+					value={firstName}
+					onChange={(e) => setFirstName(e.target.value)}
+					error={
+						firstNameError
+							? { content: 'Please enter a first name', pointing: 'below' }
+							: null
+					}
+				/>
+				<FormField
+					id="form-input-control-last-name"
+					control={Form.Input}
+					label="Last Name"
+					placeholder="Last Name"
+					value={lastName}
+					onChange={(e) => setLastName(e.target.value)}
+					error={
+						lastNameError
+							? { content: 'Please enter a last name', pointing: 'below' }
+							: null
+					}
+				/>
+				<FormField
+					id="form-input-control-email"
+					control={Form.Input}
+					label="Email"
+					placeholder="Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					error={
+						emailError
+							? { content: 'Please enter an email', pointing: 'below' }
+							: null
+					}
+				/>
+				<FormField
+					id="form-input-control-password"
+					control={Form.Input}
+					label="Password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					error={
+						passwordError
+							? { content: 'Please enter a password', pointing: 'below' }
+							: null
+					}
+				/>
+				<FormField
+					id="form-input-control-dob"
+					control={Form.Input}
+					label="Date of Birth"
+					placeholder="Date of Birth"
+					value={dob}
+					onChange={(e) => setDob(e.target.value)}
+					error={
+						dobError
+							? { content: 'Please enter a date of birth', pointing: 'below' }
+							: null
+					}
+				/>
 				<Button type="submit">Add User</Button>
 			</Form>
 		</div>

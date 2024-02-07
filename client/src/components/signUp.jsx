05b79e118229f76494b154d799336f9184a1e0c9
trnigg <tim@nigg.com.au>
2024-02-07 11:22:22 +1100

    Adds error handling and add form validation messages for confirm password

--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,7 +1,9 @@
-// TODO: Add global form validation message
+// TODO: Refactor error handling. It's spaghetti.
+// TODO: Additional error handling for email, password, (possibly date of birth) requirements is needed.
+// TODO: Add global form validation message/translate messages from Apollo.
 
 import { useState, useEffect } from 'react';
-import { Button, Form, FormField, Message } from 'semantic-ui-react';
+import { Button, Form, FormField, Message, FormGroup } from 'semantic-ui-react';
 import { MUTATION_ADDUSER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 
@@ -17,8 +19,14 @@ function SignUp({ onAuthenticated }) {
 	const [emailError, setEmailError] = useState(false);
 	const [password, setPassword] = useState('');
 	const [passwordError, setPasswordError] = useState(false);
+	const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
 	const [dob, setDob] = useState('');
 	const [dobError, setDobError] = useState(false);
+
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+	const [confirmPasswordErrorMessage, setConfirmPasswordErrorMessage] =
+		useState('');
 
 	useEffect(() => {
 		if (error) {
@@ -32,13 +40,14 @@ function SignUp({ onAuthenticated }) {
 	const handleFormSubmit = async (e) => {
 		e.preventDefault();
 
-		// Reset error states
+		// Reset error states for empty fields
 		setUserNameError(false);
 		setFirstNameError(false);
 		setLastNameError(false);
 		setEmailError(false);
 		setPasswordError(false);
 		setDobError(false);
+		setConfirmPasswordError(false);
 
 		// Check if any field is empty
 		if (!userName) setUserNameError(true);
@@ -47,6 +56,22 @@ function SignUp({ onAuthenticated }) {
 		if (!email) setEmailError(true);
 		if (!password) setPasswordError(true);
 		if (!dob) setDobError(true);
+		if (!confirmPassword) setConfirmPasswordError(true);
+
+		// Check if password and confirm password fields are both empty
+		if (!password && !confirmPassword) {
+			setPasswordError(true);
+			setConfirmPasswordError(true);
+			// Set custom error messages
+			setPasswordErrorMessage('Please enter a new password');
+			setConfirmPasswordErrorMessage('Please confirm your password');
+		} else {
+			// Check if password and confirm password fields match
+			if (password !== confirmPassword) {
+				setConfirmPasswordError(true);
+				setConfirmPasswordErrorMessage('Passwords do not match');
+			}
+		}
 
 		// If any field is empty, stop form submission
 		if (!userName || !firstName || !lastName || !email || !password || !dob)
@@ -95,35 +120,38 @@ function SignUp({ onAuthenticated }) {
 							: null
 					}
 				/>
-				<FormField
-					id="form-input-control-first-name"
-					control={Form.Input}
-					label="First Name"
-					placeholder="First Name"
-					value={firstName}
-					onChange={(e) => setFirstName(e.target.value)}
-					error={
-						firstNameError
-							? { content: 'Please enter a first name', pointing: 'below' }
-							: null
-					}
-				/>
-				<FormField
-					id="form-input-control-last-name"
-					control={Form.Input}
-					label="Last Name"
-					placeholder="Last Name"
-					value={lastName}
-					onChange={(e) => setLastName(e.target.value)}
-					error={
-						lastNameError
-							? { content: 'Please enter a last name', pointing: 'below' }
-							: null
-					}
-				/>
+				<FormGroup widths="equal">
+					<FormField
+						id="form-input-control-first-name"
+						control={Form.Input}
+						label="First Name"
+						placeholder="First Name"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+						error={
+							firstNameError
+								? { content: 'Please enter a first name', pointing: 'below' }
+								: null
+						}
+					/>
+					<FormField
+						id="form-input-control-last-name"
+						control={Form.Input}
+						label="Last Name"
+						placeholder="Last Name"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						error={
+							lastNameError
+								? { content: 'Please enter a last name', pointing: 'below' }
+								: null
+						}
+					/>
+				</FormGroup>
 				<FormField
 					id="form-input-control-email"
 					control={Form.Input}
+					type="email"
 					label="Email"
 					placeholder="Email"
 					value={email}
@@ -134,22 +162,40 @@ function SignUp({ onAuthenticated }) {
 							: null
 					}
 				/>
-				<FormField
-					id="form-input-control-password"
-					control={Form.Input}
-					label="Password"
-					placeholder="Password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					error={
-						passwordError
-							? { content: 'Please enter a password', pointing: 'below' }
-							: null
-					}
-				/>
+				<FormGroup widths="equal">
+					<FormField
+						id="form-input-control-password"
+						control={Form.Input}
+						type="password"
+						label="New Password"
+						placeholder="New Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						error={
+							passwordError
+								? { content: passwordErrorMessage, pointing: 'below' }
+								: null
+						}
+					/>
+					<FormField
+						id="form-input-control-confirm-password"
+						control={Form.Input}
+						type="password"
+						label="Confirm Password"
+						placeholder="Confirm Password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+						error={
+							confirmPasswordError
+								? { content: confirmPasswordErrorMessage, pointing: 'below' }
+								: null
+						}
+					/>
+				</FormGroup>
 				<FormField
 					id="form-input-control-dob"
 					control={Form.Input}
+					type="date"
 					label="Date of Birth"
 					placeholder="Date of Birth"
 					value={dob}

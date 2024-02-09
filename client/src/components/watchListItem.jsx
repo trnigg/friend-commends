854fb13689f9566b264304cdcd3b828e70434e1c
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2024-02-09 16:11:55 +1100

    implement remove button on watchlist

--- client/src/components/watchListItem.jsx
+++ client/src/components/watchListItem.jsx
@@ -14,15 +14,46 @@ import {
   Label,
 } from "semantic-ui-react";
 import ShareModal from "./shareModal";
+import { useMutation } from "@apollo/client";
+import {
+  MUTATION_REMOVEWATCHITEM,
+} from "../utils/selfMutations";
+import { QUERY_MYWATCHLIST } from "../utils/selfQueries";
+import Auth from "../utils/auth";
 
 export default function WatchItem(props) {
   //console.log("props:", props);
-  const { overview, AU_platforms: platforms ,type} = props;
+  const { id, overview, AU_platforms: platforms, type } = props;
   let posterURL = props?.poster_path
     ? `https://image.tmdb.org/t/p/w154/${props.poster_path}`
     : `https://react.semantic-ui.com/images/wireframe/image.png`;
   let title = props.original_title || props.original_name;
-  const itemType = type==="TV"? "TV show":type;
+  const itemType = type === "TV" ? "TV show" : type;
+
+  const [removeWatchItem, { error: movieErr }] = useMutation(MUTATION_REMOVEWATCHITEM,{
+    refetchQueries: [
+      QUERY_MYWATCHLIST, // Query to be refecthed
+      'MyWatchList' // Query name
+    ],
+  });
+  const handleRemoveOnClick = async () => {
+    //console.log("props.share",props.share)
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+      try {
+        await removeWatchItem({
+          variables: { removeFromWatchListId:id },
+        });
+      } catch (err) {
+        console.error(err);
+      }
+
+
+  };
 
   return (
     <Item>
@@ -31,15 +62,21 @@ export default function WatchItem(props) {
       <ItemContent>
         <ItemHeader as="a">{title}</ItemHeader>
         <ItemMeta>
-            <span className="type"> {itemType} </span>
+          <span className="type"> {itemType} </span>
         </ItemMeta>
         <ItemDescription>{overview}</ItemDescription>
         <ItemExtra>
           {platforms?.map((platform) => (
             <Label key={platform}>{platform}</Label>
           ))}
-          <ShareModal key={props.id} {...props}/>
+          <ShareModal key={props.id} {...props} />
+          <div>
+          <Button primary onClick={() => handleRemoveOnClick()}>
+          Remove from list
+        </Button>
+        </div>
         </ItemExtra>
+
       </ItemContent>
     </Item>
   );

c8a846949becb0f36ad4cb387aa6d9a92b0ccd19
trnigg <tim@nigg.com.au>
2024-02-09 17:42:31 +1100

    Adds useReducer hook and SelectedContent component to TVShows page

--- client/src/pages/TVShows.jsx
+++ client/src/pages/TVShows.jsx
@@ -13,10 +13,11 @@
 // NOTE: These are the known poster_path sizes: "w92", "w154", "w185", "w342", "w500", "w780", or "original"
 // so combined, for mobile eg http://image.tmdb.org/t/p/w185/ + poster_path
 
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { Search } from 'semantic-ui-react';
 import { searchTVsByQuery, searchTVByID, searchTVSource } from '../utils/API';
 import ContentSearch from '../components/contentSearch';
+import SelectedContent from '../components/selectedContent';
 import {
 	Header,
 	HeaderContent,
@@ -33,7 +34,30 @@ import {
 	CardMeta,
 } from 'semantic-ui-react';
 
+const initialState = {
+	loading: false,
+	results: [],
+	value: '',
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'CLEAN_QUERY':
+			return initialState;
+		case 'START_SEARCH':
+			return { ...state, loading: true, value: action.query };
+		case 'FINISH_SEARCH':
+			return { ...state, loading: false, results: action.results };
+		case 'UPDATE_SELECTION':
+			return { ...state, value: action.selection };
+
+		default:
+			throw new Error();
+	}
+}
+
 function TVShows() {
+	const [state, dispatch] = useReducer(reducer, initialState);
 	const [selectedTVShow, setSelectedTVShow] = useState(null);
 	const [tvShowSource, setTVShowSource] = useState(null);
 
@@ -52,7 +76,6 @@ function TVShows() {
 	const handleSearch = async (query) => {
 		try {
 			const data = await searchTVsByQuery(query);
-			console.log('data', data); // Log the parsed data
 			return data; // return the parsed data
 		} catch (error) {
 			console.error('Failed to search TVs by query', error);
@@ -60,68 +83,30 @@ function TVShows() {
 	};
 
 	const handleSelectTVShow = async (e, data) => {
+		console.log(data); // Log the data
+
 		// If the selected result is the "Keep typing..." message, do nothing
-		if (data.result.title === 'Keep typing to see more results...') {
+		if (data.title === 'Keep typing to see more results...') {
 			return;
 		}
-		// reset the active index to close the accordion panels
-		setActiveIndex(-1);
-		dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title });
-		setSelectedTVShow(data.result);
 
-		const source = await searchTVSource(data.result.id);
-		// By default, we will use the Australian region
-		// In the future, we can add a user setting to change this and add it to the user's schema
+		setActiveIndex(-1);
+		dispatch({ type: 'UPDATE_SELECTION', selection: data.title });
+		setSelectedTVShow(data);
+
+		const source = await searchTVSource(data.id);
 		const userRegion = 'AU';
 
-		// If there are results for the user's region, set the TVShowSource state
-		// Otherwise, set the noStreamingAvailable state to true
-		// this will display a message to the user
 		if (source.results[userRegion]) {
-			// Sort the providers by display priority, a default field from the API
-			// lower numbers are higher priority
 			const providers = source.results[userRegion].flatrate;
 			providers.sort((a, b) => a.display_priority - b.display_priority);
-			setTVShowSource(providers);
-			setNoStreamingAvailable(false); // Reset the noStreamingAvailable state
+			setContentSource(providers);
+			setNoStreamingAvailable(false);
 		} else {
-			setTVShowSource([]);
-			setNoStreamingAvailable(true); // Set the noStreamingAvailable state to true
+			setContentSource([]);
+			setNoStreamingAvailable(true);
 		}
 	};
-
-	// panels for the accordion
-	// requires ternary to check if the selectedTVShow is null;
-	// otherwise it will throw an error before a TV show is selected
-	// renders the description and streaming providers, if available, else displays a message
-	const panels = [
-		{
-			key: 'description',
-			title: 'Description',
-			content: selectedTVShow ? selectedTVShow.description : '',
-		},
-		{
-			key: 'stream',
-			title: 'Stream',
-			content: {
-				content: noStreamingAvailable
-					? 'This content is not available in your region.'
-					: tvShowSource &&
-					  tvShowSource.map((provider) => (
-							<Label
-								key={provider.provider_id}
-								image
-								style={{ marginBottom: '4px' }}
-							>
-								<img
-									src={`http://image.tmdb.org/t/p/w92/${provider.logo_path}`}
-								/>
-								{provider.provider_name}
-							</Label>
-					  )),
-			},
-		},
-	];
 
 	return (
 		<Container>
@@ -131,66 +116,19 @@ function TVShows() {
 					<Icon name="tv" />
 					<HeaderContent>Search for a show</HeaderContent>
 				</Header>
-				<ContentSearch handleSearch={handleSearch} />
-				{selectedTVShow && (
-					<Card>
-						<CardContent>
-							<Card.Header>{selectedTVShow.title}</Card.Header>
-						</CardContent>
-						{selectedTVShow.backdropImage && (
-							<Image
-								src={`http://image.tmdb.org/t/p/w342/${selectedTVShow.backdropImage}`}
-								wrapped
-								ui={false}
-								rounded // This is not working
-								style={{
-									marginLeft: '15px',
-									marginRight: '15px',
-									marginBottom: '15px',
-									borderRadius: '5px',
-								}}
-							/>
-						)}
-						<CardMeta
-							style={{
-								display: 'flex',
-								alignItems: 'center',
-								marginLeft: '15px',
-								marginRight: '15px',
-								marginBottom: '15px',
-							}}
-						>
-							<Icon
-								name="thumbs up outline"
-								size="big"
-								style={{ marginRight: '10px' }}
-							/>
-							<div>
-								{' '}
-								Recommended by <strong>name1</strong>, <strong>name2</strong>{' '}
-								and <strong>4 other friends</strong>.
-							</div>
-						</CardMeta>
-						<CardContent extra>
-							<Accordion
-								activeIndex={activeIndex}
-								onTitleClick={(e, { index }) =>
-									setActiveIndex(index === activeIndex ? -1 : index)
-								}
-								defaultActiveIndex={-1}
-								panels={panels}
-							/>
-						</CardContent>
-						<CardContent
-							extra
-							style={{ display: 'flex', justifyContent: 'center', gap: '15px' }}
-						>
-							<Button circular icon="thumbs up" size="big" />
-							<Button circular icon="share" size="big" />
-							<Button circular icon="add" size="big" />
-						</CardContent>
-					</Card>
-				)}
+				<ContentSearch
+					state={state}
+					dispatch={dispatch}
+					handleSearch={handleSearch}
+					onResultSelect={handleSelectTVShow}
+				/>
+				<SelectedContent
+					selectedContent={selectedTVShow}
+					source={tvShowSource}
+					noStreamingAvailable={noStreamingAvailable}
+					activeIndex={activeIndex}
+					setActiveIndex={setActiveIndex}
+				/>
 			</Segment>
 		</Container>
 	);

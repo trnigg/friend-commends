244320aa2ee99381dd649cdc0c82865ce8b79a1d
trnigg <tim@nigg.com.au>
2024-02-11 11:20:06 +1100

    Refactors core ForYou UI

--- client/src/pages/ForYou.jsx
+++ client/src/pages/ForYou.jsx
@@ -1,18 +1,22 @@
-import { Button } from 'semantic-ui-react';
+import {
+	Container,
+	Segment,
+	Header,
+	Message,
+	HeaderContent,
+	HeaderSubheader,
+	Icon,
+} from 'semantic-ui-react';
+
 import auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
-import { Header } from 'semantic-ui-react';
 import { QUERY_USER } from '../utils/queries';
 import CardExampleCard from '../components/CardPics';
 import './Nav_Page.css';
 import { QUERY_FRIENREQ } from '../utils/queries';
 // import { useEffect, useState } from 'react';
 
-
-
-
 function ForYou() {
-
 	// const [type, setType] = useState()
 	// const [funnArray, setFunArray] = useState()
 	// const handleClick = (workingArray) => {
@@ -20,35 +24,38 @@ function ForYou() {
 	// 							setFunArray(workingArray)
 	// 							console.log(funnArray)
 	// 							}
-  
+
 	// useEffect(() => {
 	//   console.log('You are ' + type + ' years old!')
 	// })
 
-	console.log("Hello")
+	console.log('Hello');
 
 	let userDetails = {};
 	let friendsDetails = {};
 	const idNum = auth.getProfile();
-	console.log(idNum.data._id)
-	const { loading: loading2, error: error2, data: data2 } = useQuery(QUERY_FRIENREQ, {
+	console.log(idNum.data._id);
+	const {
+		loading: loading2,
+		error: error2,
+		data: data2,
+	} = useQuery(QUERY_FRIENREQ, {
 		variables: { userId: idNum.data._id },
-
-	})
+	});
 	const { loading, error, data } = useQuery(QUERY_USER, {
 		variables: { userId: idNum.data._id },
 	});
-	if (loading||loading2) {
+	if (loading || loading2) {
 		return <div>Please Wait.....</div>;
 	}
-	if (data&&data2)  {
+	if (data && data2) {
 		userDetails = data;
-		console.log("Yellow")
+		console.log('Yellow');
 		friendsDetails = data2.friendRecommendations;
 		const friendArray = [];
-		friendsDetails.forEach((entry)=>{
-			friendArray.push(entry.recommendations)
-		})
+		friendsDetails.forEach((entry) => {
+			friendArray.push(entry.recommendations);
+		});
 		const workingArray = friendArray.flat();
 		const movieRecommend = workingArray
 
@@ -63,71 +70,82 @@ function ForYou() {
 		console.log(movieRecommend, tvRecommend, bookRecommend);
 
 		return (
-			<div>
-				<Header as="h1">Welcome {userDetails.user.userName} to your profile page</Header>
-				<div>
+			<Container>
+				<Segment>
+					<Header as="h1">For You</Header>
+				</Segment>
 
-					<h2>
-						You have {userDetails.user.pendingFriendRequests.length} friend
-						requests
-					</h2>
-
-					<Header as="h2">Friends Movie Recommendations</Header>
-					<div className="movieBox">
-						<>
-							{movieRecommend.map(function (data) {
-								return (
-									<div key={data.original_title}>
-										<CardExampleCard 
-										movietitle={data.original_title} 
-										poster_path={data.poster_path}
-                                        description={data.overview}
-										/>
-									</div>
-								);
-							})}
-						</>
-					</div>
-					<Button as='a' href='/Movies'>See more movie recommendations</Button>
-
-					<Header as="h2">Friends TV Show Recommendations</Header>
-					<div className="tvBox">
-						<>
-							{tvRecommend.map(function (data) {
-								return (
-									<div key={data.original_name}>
-										<CardExampleCard 
-										movietitle={data.original_name}
-                                        poster_path={data.poster_path}
-                                        description={data.overview}
-										/>
-									</div>
-								);
-							})}
-						</>
-					</div>
-					<Button as='a' href='/tv_shows'>See more TV recommendations</Button>
-
-					<Header as="h2">Books</Header>
-					<div className="bookBox">
-						<>
-							{bookRecommend.map(function (data) {
-								return (
-									<div key={data.original_name}>
-										<CardExampleCard
-										movietitle={data.original_name}
-                                        poster_path={data.poster_path}
-                                        description={data.overview}
-										/>
-									</div>
-								);
-							})}
-						</>
-					</div>
-
-					<Button as='a' href='/books'>See Recommended</Button>
-				</div>
-			</div>
+				<Message>
+					You have {userDetails.user.pendingFriendRequests.length} friend
+					requests
+				</Message>
+				<Segment>
+					<Header as="h2">
+						<Icon name="film" />
+						<HeaderContent>
+							Movies recommended for you
+							<HeaderSubheader>
+								<a href="/Movies">See more movie recommendations...</a>
+							</HeaderSubheader>
+						</HeaderContent>
+					</Header>
+					{movieRecommend.map(function (data) {
+						return (
+							<div key={data.original_title}>
+								<CardExampleCard
+									movietitle={data.original_title}
+									poster_path={data.poster_path}
+									description={data.overview}
+								/>
+							</div>
+						);
+					})}
+				</Segment>
+				<Segment>
+					<Header as="h2">
+						<Icon name="tv" />
+						<HeaderContent>
+							TV shows recommended for you
+							<HeaderSubheader>
+								<a href="/tv_shows">See more TV recommendations...</a>
+							</HeaderSubheader>
+						</HeaderContent>
+					</Header>
+					{tvRecommend.map(function (data) {
+						return (
+							<div key={data.original_name}>
+								<CardExampleCard
+									movietitle={data.original_name}
+									poster_path={data.poster_path}
+									description={data.overview}
+								/>
+							</div>
+						);
+					})}
+				</Segment>
+				<Segment>
+					<Header as="h2">
+						<Icon name="book" />
+						<HeaderContent>
+							Books recommended for you
+							<HeaderSubheader>
+								<a href="/books">See more book recommendations...</a>
+							</HeaderSubheader>
+						</HeaderContent>
+					</Header>
+					{bookRecommend.map(function (data) {
+						return (
+							<div key={data.original_name}>
+								<CardExampleCard
+									movietitle={data.original_name}
+									poster_path={data.poster_path}
+									description={data.overview}
+								/>
+							</div>
+						);
+					})}
+				</Segment>
+			</Container>
 		);
 	} else {
 		console.log('No Luck');
